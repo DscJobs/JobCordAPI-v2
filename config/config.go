f30cec101eb01bf73a7058b34acc6fd394c46df5
Your Name <you@example.com>
2023-03-13 10:21:32 +0000

    .

--- config/config.go
+++ config/config.go
@@ -2,12 +2,12 @@ package config
 
 type Config struct {
 	Token        string                 `yaml:"token" comment:"Discord token" validate:"required"`
-	LoginClients map[string]LoginClient `yaml:"login_clients" comment:"Login clients" validate:"required"`
 	PostgresURL  string                 `yaml:"postgres_url" default:"postgresql:///dscjobs" comment:"Postgres URL" validate:"required"`
 	RedisURL     string                 `yaml:"redis_url" default:"redis://localhost:6379/2" comment:"Redis URL" validate:"required"`
 	Port         string                 `yaml:"port" default:":5848" comment:"Port to run the server on" validate:"required"`
 	APIUrl       string                 `yaml:"api_url" default:"https://api.jobcord.co" comment:"URL of the API" validate:"required"`
 	PopplioURL   string                 `yaml:"popplio_url" default:"http://127.0.0.1:8081" comment:"URL of the Popplio server" validate:"required"`
+	LoginClients map[string]LoginClient `yaml:"login_clients" comment:"Login clients" validate:"required"`
 }
 
 type LoginClient struct {
